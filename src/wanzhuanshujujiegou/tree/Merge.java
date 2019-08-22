0575b22f775fdf27d143d1e55bb08e9fe0e4c832
WatchingWind <1992353533@qq.com>
2019-08-22 11:31:21 +0800

    阶段性保存

---
+++ src/wanzhuanshujujiegou/tree/Merge.java
@@ -0,0 +1,5 @@
+package wanzhuanshujujiegou.tree;
+
+public interface Merge<E> {
+    public E merge(E e1,E e2);
+}
--- src/wanzhuanshujujiegou/tree/SegmentTree.java
+++ src/wanzhuanshujujiegou/tree/SegmentTree.java
@@ -3,13 +3,33 @@ package wanzhuanshujujiegou.tree;
 public class SegmentTree <E>{
     private E[] data;
     private E[] tree;
+    Merge<? super E> merger ;
 
-    public SegmentTree(E[] arr){
+    public SegmentTree(E[] arr,Merge<? super E> merge){
+        this.merger = merge;
         data = (E[]) new Object[arr.length];
         for(int i = 0; i < arr.length; i++ ) {
             data[i] = arr[i];
         }
         tree = (E[]) new Object[4 * arr.length];
+
+        buildSegmentTree(0,0,data.length - 1);
+    }
+
+    private void buildSegmentTree(int treeIndex, int l, int r) {
+        if(l == r ) {
+            tree[treeIndex] = data[l];
+            return;
+        }
+
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+
+        int mid = l + ( r - l) /2;
+        buildSegmentTree(leftTreeIndex, l, mid);
+        buildSegmentTree(rightTreeIndex, mid + 1, r);
+
+        tree[treeIndex] = (E)merger.merge(tree[leftTreeIndex],tree[rightTreeIndex]);
     }
 
     public E get(int index){
@@ -30,5 +50,4 @@ public class SegmentTree <E>{
     private  int rightChild(int index){
         return 2 * index + 2;
     }
-
 }
